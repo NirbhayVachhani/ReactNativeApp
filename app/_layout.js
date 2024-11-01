a78503a9454f35b642c9b21e61153db16eff41f7
NIRBHAY ASHVINBHAI VACHHANI <nirbhayvachhani@gmail.com>
2024-10-31 19:20:21 -0600

    UPDATED

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,22 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
+import { Button } from "react-native-web";
+import { router } from "expo-router";
 
 const Layout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack>
       <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="model" options={{ title: "Model", presentation: "modal"}} />
+      <Stack.Screen name="register" options={{ title: "Register",
+      headerRight: () => (<Button title="open" onPress={() => router.push('model')} />)
+       }} />
+      <Stack.Screen name="model" 
+      options={{ title: "Model",
+         presentation: "modal",
+         animation: "flip",
+         headerLeft: () => (<Button title="close" onPress={() => router.back()} />)
+       }} />
       <Stack.Screen name="(Drawer)" options={{ title: "About" }} />
     </Stack>
   );
